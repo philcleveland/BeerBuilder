f80d27d08d23c24e4ccfd5c08121d7b602fd8379
Phil Cleveland <pdoh00@gmail.com>
2014-11-15 15:01:43 -0800

    added a wpf client.

---
+++ BrewDay.WPF.Client/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BrewDay.WPF.Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            var ingrediants =new List<Tuple<double,double>>();
+            ingrediants.Add(new Tuple<double,double>(2.0, 13.75));
+            ingrediants.Add(new Tuple<double, double>(22.5, 0.25));
+            ingrediants.Add(new Tuple<double, double>(23.75, 0.1875));
+
+            var mcu = BeerBuilder.Functions.mcu(ingrediants.ToArray(), 5.0);
+            var srm = (int)Math.Round(BeerBuilder.Functions.srm_color(mcu));
+
+            var color = BeerBuilder.Functions.srmColorLookup.TryFind(srm);
+
+            this.DataContext = new MainWindowViewModel(color.Value);
+        }
+    }
+}
---
+++ BrewDay.WPF.Client/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace BrewDay.WPF.Client
+{
+    public class MainWindowViewModel
+    {
+        //public MainWindowViewModel()
+        //{
+        //    SRMColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#f8a600")); 
+        //}
+
+        public MainWindowViewModel(int color)
+        {
+            var bytes = BitConverter.GetBytes(color);
+            var dispColor = Color.FromRgb(bytes[3], bytes[2], bytes[1] );
+            var colorString = string.Format("#{0:X2}{1:X2}{2:X2}", bytes[2], bytes[1], bytes[0]);
+            this.SRMColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString(colorString));
+        }
+
+        public SolidColorBrush SRMColor { get; set; }
+    }
+}
