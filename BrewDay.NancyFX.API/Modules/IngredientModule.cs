d749bb4ab6efb222291a749708777b3fc510f179
Phil Cleveland <pdoh00@gmail.com>
2014-11-21 21:28:00 -0800

    updated README. Updated web svc with method to rebuild db data

--- BrewDay.NancyFX.API/Modules/IngredientModule.cs
+++ BrewDay.NancyFX.API/Modules/IngredientModule.cs
@@ -3,6 +3,7 @@ using Nancy;
 using Raven.Client;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 
@@ -21,9 +22,25 @@ namespace BrewDay.NancyFX.API.Modules
                 //each ingredient should contains links with the following rel values:
                 //self
 
+
                 //convert to collection+json
                 return await session.Query<Ingredient>().ToListAsync();
             };
+
+            Post["/rebuild", true] = async (x, y) =>
+                {
+                    using (var fs = new FileStream(@"..\..\BeerBuilder\Malts.csv", FileMode.Open, FileAccess.Read))
+                    {
+                        var malts = BeerBuilder.Malts.GetMalts(fs);
+                        foreach (var m in malts)
+                        {
+                            await session.StoreAsync(new Ingredient() { Name = m.Name, Type = m.Type, PPG = m.PPG, ColorLower = m.ColorLower, ColorUpper = m.ColorUpper });
+                        }
+                    }
+                    await session.SaveChangesAsync();
+
+                    return HttpStatusCode.OK;
+                };
         }
     }
 }
--- README.md
+++ README.md
@@ -2,3 +2,7 @@ BeerBuilder
 ===========
 
 Going to use F# to recreate BrewerFriend calcs and possibly move into creating a BeerSmith type app
+
+Brew calculations being exposed via a NancyFX web service
+
+Using RavenDB to store data.  Need to have RavenServer running at http://localhost:8080 for anything to work
